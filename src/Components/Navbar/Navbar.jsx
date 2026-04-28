da46cdcfc7909cf59ebeebb21c9830b77f8c9869
Sameera Udesh <sameeraudesh75@gmail.com>
2026-04-28 19:34:03 +0530

    add

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import './Navbar.css'
-import navProfile from '../../assets/nav-profile.svg'
+import navProfile from '../../assets/user-setting.png'
 import navlogo from '../../assets/nav-logo.svg'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
-    <img src={navlogo} alt="" className="nav-logo" />
-    <img src={navProfile} className='nav-profile' alt="" />
+    <div className="navbar">
+
+      <div className="navbar-left">
+        <img src={navlogo} alt="Logo" className="nav-logo" />
+        <span className="nav-brand">Admin Panel</span>
+      </div>
+
+      <div className="navbar-right">
+        <div className="nav-profile-wrapper">
+          <img src={navProfile} className="nav-profile" alt="Profile" />
+          <div className="nav-profile-info">
+            <span className="nav-profile-name">Admin</span>
+            <span className="nav-profile-role">Super Admin</span>
+          </div>
+        </div>
+      </div>
 
     </div>
   )
